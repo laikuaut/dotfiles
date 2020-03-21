ba8bdd00f017fd93e09ea13176534b7dd005d16e
laikuaut <laikuaut@gmail.com>
2020-03-22 04:04:22 +0900

    コメント追加

--- bash_profile.sh
+++ bash_profile.sh
@@ -1,4 +1,6 @@
 # bash_profile
 
+# ロケールを設定
 export LANG="ja_JP.utf8"
+# タイムゾーンを設定（JST）
 export TZ="Asia/Tokyo"
--- bashrc.sh
+++ bashrc.sh
@@ -5,6 +5,7 @@ alias pwd="pwd -P"
 alias ll="ls -la"
 alias vi="vim"
 
+# 絶対パスのファイル一覧取得
 function lsa() {
   for file in `ls`
   do
